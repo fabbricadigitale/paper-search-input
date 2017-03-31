46c0eabce0c56ce522f3241b78237a896f3a9769
Alessandro La Bella [fabbricadigitale] <a.labella@fabbricadigitale.it>
2017-03-31 18:20:26 +0200

    New: Basic UI behavior

--- scripts/paper-search-bar.js
+++ scripts/paper-search-bar.js
@@ -20,7 +20,37 @@ class PaperSearchBar extends Polymer.mixinBehaviors(Polymer.IronControlState, Po
       placeholder: {
         type: String,
         value: 'Search'
+      },
+
+      collapseView: {
+        type: Boolean,
+        value: false
+      },
+
+      fullbleed: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+        observer: "_whenFullbleedChanged"
+      },
+
+      wide: {
+        type: Boolean,
+        value: false,
+        observer: "_whenWideChanged"
+      },
+
+      _showInput: {
+        type: Boolean,
+        value: false,
+        computed: "_computeShowInput(wide, fullbleed)"
+      },
+
+      _showSearch: {
+        type: Boolean,
+        value: true
       }
+
     }
   }
 
@@ -31,5 +61,37 @@ class PaperSearchBar extends Polymer.mixinBehaviors(Polymer.IronControlState, Po
   _clear() {
     this.query = ''
   }
+
+  _activeSearch(evt) {
+    if(this.wide) {
+      console.log("Wide view")
+    }
+    if(!this.wide) {
+      console.log("Collapse view")
+      this.fullbleed = true;
+    }
+  }
+
+  _deactiveSearch(evt) {
+    if(!this.wide) {
+      this.fullbleed = false;
+    }
+  }
+
+  _computeShowInput(wide, fullbleed) {
+    if(wide) return true
+    if(!wide && !fullbleed ) return false
+    if(!wide && fullbleed) return true
+    return false
+  }
+
+  _whenFullbleedChanged(fullbleed) {
+    this._showSearch = !fullbleed
+  }
+
+  _whenWideChanged(wide) {
+    if(wide) this.fullbleed = false
+  }
+
 }
 customElements.define(PaperSearchBar.is, PaperSearchBar)
